c46af58a0dedf0937c5941383328af61f4a01dfb
Jackson Fraser <s3529589@student.rmit.edu.au>
2017-04-11 19:49:15 +1000

    Added some classes to model

---
+++ src/model/GameEngineCallbackImpl.java
@@ -0,0 +1,46 @@
+package model;
+
+import model.interfaces.GameEngine;
+import model.interfaces.GameEngineCallback;
+import model.interfaces.Player;
+import model.interfaces.PlayingCard;
+
+public class GameEngineCallbackImpl implements GameEngineCallback {
+
+	@Override
+	public void nextCard(Player player, PlayingCard card, GameEngine engine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bustCard(Player player, PlayingCard card, GameEngine engine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void result(Player player, int result, GameEngine engine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void nextHouseCard(PlayingCard card, GameEngine engine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void houseBustCard(PlayingCard card, GameEngine engine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void houseResult(int result, GameEngine engine) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
